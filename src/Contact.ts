fdd6a69b61292cfe787351646c5985d355d5872d
Michiel de Jong <michiel@unhosted.org>
2020-04-07 09:55:36 +0200

    basic attempt in place; running into Failing attempt to construct URL

--- src/Contact.ts
+++ src/Contact.ts
@@ -1,15 +1,171 @@
-import { TripleSubject } from "plandoc";
-import { StateTransition } from "snap-checker";
+import {
+  VirtualSubject,
+  VirtualContainer,
+  // describeContainer,
+  // Reference,
+  TripleDocument,
+  fetchDocument,
+  describeDocument,
+  VirtualDocument,
+  describeSubject,
+  describeContainer
+} from "plandoc";
+import { StateTransition, SnapChecker } from "snap-checker";
+// import { internal_fetchContainer } from "plandoc/dist/actors/container";
+import { snapMessageToWeb, snapMessageFromWeb } from "./message";
+import { ldp } from "rdf-namespaces";
+
+// copied from
+// https://github.com/inrupt/friend-requests-exploration/blob/master/src/services/usePersonDetails.ts
+const as = {
+  following: "https://www.w3.org/TR/activitypub/#following"
+};
+
+const snap = {
+  ourInbox: "https://ledgerloops.com/snap/#our-in",
+  ourOutbox: "https://ledgerloops.com/snap/#our-out",
+  theirInbox: "https://ledgerloops.com/snap/#their-in",
+  root: "https://ledgerloops.com/snap/#root"
+};
 
 export class Contact {
-  addressbookEntry: TripleSubject;
-  constructor(addressbookEntry: TripleSubject) {
+  addressbookEntry: VirtualSubject;
+  snapChecker: SnapChecker;
+  snapRoot: VirtualContainer;
+  ourName: string;
+  theirName: string;
+  unit: string;
+  constructor(
+    addressbookEntry: VirtualSubject,
+    snapRoot: VirtualContainer,
+    ourName: string,
+    theirName: string,
+    unit: string
+  ) {
     this.addressbookEntry = addressbookEntry;
-    this.snapChecker = new SnapChecker();
+    this.snapChecker = new SnapChecker([]);
+    this.snapRoot = snapRoot;
+    this.ourName = ourName;
+    this.theirName = theirName;
+    this.unit = unit;
   }
-  sendMessage(msg: StateTransition) {}
-  fetchSentMessages() {}
-  fetchReceivedMessages() {}
-  fetchMessages() {}
-  subscribeToReceivedMessage() {}
+
+  async ensureMessageBoxUrl(predicate: string): Promise<TripleDocument> {
+    console.log("Ensuring", predicate);
+    // FIXME: Use https://gitlab.com/vincenttunru/plandoc/-/issues/8#note_318442264 here
+    const box: VirtualDocument = describeDocument().isEnsuredOn(
+      this.addressbookEntry,
+      predicate,
+      this.snapRoot
+    );
+    console.log("Fetching", box);
+    return fetchDocument(box);
+  }
+
+  async sendMessageTo(
+    msg: StateTransition,
+    messageBoxPredicate: string
+  ): Promise<void> {
+    // const ourOutbox: TripleContainer = this.ensureMessageBoxUrl(snap.ourOutbox);
+    // const ourOutboxDoc = createDocumentInContainer(ourOutbox);
+    const box: TripleDocument = await this.ensureMessageBoxUrl(
+      messageBoxPredicate
+    );
+    await snapMessageToWeb(msg, box);
+  }
+
+  async sendMessage(msg: StateTransition): Promise<void> {
+    this.sendMessageTo(msg, snap.ourOutbox);
+    this.sendMessageTo(msg, snap.theirInbox);
+  }
+
+  async fetchMessagesFrom(
+    messageBoxPredicate: string,
+    from: string,
+    to: string,
+    unit: string
+  ): Promise<void> {
+    console.log("ensuring", messageBoxPredicate);
+    const box: TripleDocument = await this.ensureMessageBoxUrl(
+      messageBoxPredicate
+    );
+    const boxSub = box.getSubject("");
+    const docs: string[] = boxSub.getAllRefs(ldp.contains);
+    console.log("fetchMessagesFrom", messageBoxPredicate, from, to, unit);
+    const promises: Promise<void>[] = docs.map(
+      async (msgDocUrl: string): Promise<void> => {
+        const thisVirtualDoc = describeDocument().isFoundAt(msgDocUrl);
+        console.log("Fetching", msgDocUrl);
+        const thisDoc = await fetchDocument(thisVirtualDoc);
+        const snapMessage = await snapMessageFromWeb(thisDoc);
+        this.snapChecker.processMessage({
+          from,
+          to,
+          unit,
+          stateTransition: snapMessage,
+          time: new Date()
+        });
+      }
+    );
+    await Promise.all(promises);
+  }
+
+  async fetchSentMessages(): Promise<void> {
+    console.log("fetchSentMessages");
+    await this.fetchMessagesFrom(
+      snap.ourOutbox,
+      this.ourName,
+      this.theirName,
+      this.unit
+    );
+  }
+  async fetchReceivedMessages(): Promise<void> {
+    console.log("fetchReceivedMessages");
+    await this.fetchMessagesFrom(
+      snap.ourInbox,
+      this.ourName,
+      this.theirName,
+      this.unit
+    );
+  }
+
+  async fetchMessages(): Promise<void> {
+    console.log("fetchMessages");
+    await Promise.all([
+      this.fetchReceivedMessages()
+      // this.fetchSentMessages()
+    ]);
+  }
+  async subscribeToReceivedMessage(): Promise<void> {
+    //
+  }
+}
+
+export async function fetchContacts(
+  sessionWebId: string
+): Promise<{ [webId: string]: Contact }> {
+  const addressbookEntry = describeSubject().isFoundAt("");
+  const profileSub = describeSubject().isFoundAt(sessionWebId);
+  const snapRoot = describeContainer().isFoundOn(profileSub, snap.root);
+  if (sessionWebId === "https://lolcathost.de/storage/alice/profile/card#me") {
+    return {
+      "https://lolcathost.de/storage/bob/profile/card#me": new Contact(
+        addressbookEntry,
+        snapRoot,
+        "alice",
+        "bob",
+        "1E-6 USD"
+      )
+    };
+  } else {
+    return {
+      "https://lolcathost.de/storage/alice/profile/card#me": new Contact(
+        addressbookEntry,
+        snapRoot,
+        "bob",
+        "alice",
+        "1E-6 USD"
+      )
+    };
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,5 @@
-import { Contact, sendMessage } from "./sendMessage";
 import { SnapTransactionState } from "snap-checker";
-import {
-  VirtualSubject,
-  describeSubject,
-  VirtualContainer,
-  describeContainer
-} from "plandoc";
-import { internal_fetchContainer } from "plandoc/dist/actors/container";
-import { vcard } from "rdf-namespaces";
-
-// copied from
-// https://github.com/inrupt/friend-requests-exploration/blob/master/src/services/usePersonDetails.ts
-const as = {
-  following: "https://www.w3.org/TR/activitypub/#following"
-};
-const snap = {
-  ourInbox: "https://ledgerloops.com/snap/#our-in",
-  ourOutbox: "https://ledgerloops.com/snap/#our-out",
-  theirInbox: "https://ledgerloops.com/snap/#their-in"
-};
-
-// there are two tasks, reading from message containers,
-// and posting to message containers.
-// Then there is the current user's addressbook.
-// Maybe I want to create a Contact class with private variables for all three containers.
-
-async function createMessageContainers(contactWebId: string) {
-  const contactProfile;
-}
-async function loadMessages(contact: Contact): Promise<void> {
-  const virtualContainerbox = describeContainer().isFoundOn(
-    contact.ourSentboxUrl
-  );
-  const sentBoxDoc = await getDocument(contact.ourSentboxUrl);
-  const inboxDoc = await getDocument(contact.ourInboxUrl);
-  console.log(sentBoxDoc.getStatements(), inboxDoc.getStatements());
-}
+import { fetchContacts, Contact } from "./Contact";
 
 window.onload = (): void => {
   console.log("document ready");
@@ -45,7 +9,7 @@ window.onload = (): void => {
         trackSession: (callback: (session: { webId: string }) => void) => void;
       };
     };
-  }).solid.auth.trackSession((session: { webId: string }) => {
+  }).solid.auth.trackSession(async (session: { webId: string }) => {
     if (!session) {
       console.log("The user is not logged in");
       document.getElementById(
@@ -54,69 +18,22 @@ window.onload = (): void => {
       document.getElementById("ui").style.display = "none";
     } else {
       console.log(`Logged in as ${session.webId}`);
-      let contacts: { [webId: string]: Contact };
-      if (
-        session.webId === "https://lolcathost.de/storage/alice/profile/card#me"
-      ) {
-        const profile: VirtualSubject = describeSubject().isFoundAt(
-          session.webId
-        );
-        const friendsGroup: VirtualSubject = describeSubject().isEnsuredOn(
-          profile,
-          as.following
-        );
-        const firstFriend: VirtualSubject = describeSubject().isFoundOn(
-          friendsGroup,
-          vcard.hasMember
-        );
-        const ourOutbox: VirtualContainer = describeContainer().isEnsuredOn(
-          firstFriend,
-          snap.ourOutbox
-        );
-        const ourInbox: VirtualContainer = describeContainer().isEnsuredOn(
-          firstFriend,
-          snap.ourInbox
-        );
-        const theirInbox: VirtualContainer = describeContainer().isEnsuredOn(
-          firstFriend,
-          snap.theirInbox
-        );
-        contacts = {
-          "https://lolcathost.de/storage/bob/profile/card#me": {
-            ourOutbox,
-            ourInbox,
-            theirInbox
-          }
-        };
-      }
-      if (
-        session.webId === "https://lolcathost.de/storage/bob/profile/card#me"
-      ) {
-        contacts = {
-          "https://lolcathost.de/storage/alice/profile/card#me": {
-            ourSentboxUrl: "https://lolcathost.de/storage/bob/snap/out/alice/",
-            ourInboxUrl: "https://lolcathost.de/storage/bob/snap/in/alice/",
-            theirInboxUrl: "https://lolcathost.de/storage/alice/snap/in/bob/"
-          }
-        };
-      }
-      Object.keys(contacts).forEach((contact: string) => {
-        console.log("Loading bilateral message history", contact);
-        loadMessages(contacts[contact]);
-      });
-      (window as any).getDocument = getDocument;
+      const contacts: { [webId: string]: Contact } = await fetchContacts(
+        session.webId
+      );
+      // (window as any).getDocument = getDocument;
       (window as any).sendSomething = async (): Promise<void> => {
         await Promise.all(
-          Object.keys(contacts).map((webId: string) => {
-            console.log("Loading bilateral message history", contacts[webId]);
-            return sendMessage(
-              {
-                transId: 1,
-                newState: SnapTransactionState.Proposing,
-                amount: 20
-              },
-              contacts[webId]
-            );
+          Object.keys(contacts).map(async (webId: string) => {
+            const contact = contacts[webId];
+            console.log("Loading bilateral message history", contact);
+            await contact.fetchMessages();
+            // console.log("Sending a message", contact);
+            // return contact.sendMessage({
+            //   transId: 1,
+            //   newState: SnapTransactionState.Proposing,
+            //   amount: 20
+            // });
           })
         );
       };
